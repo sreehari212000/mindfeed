ddf99e9b2c9587e90736ae0b2b1f527e019dc916
Sreehari J <sreeharijayaram21@gmail.com>
2025-08-14 20:37:32 +0530

    Design and implement sign in page

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div className='h-[93vh] flex justify-center items-center'>
+      <div className="border rounded-md flex flex-col items-center p-10 w-full max-w-md glass backdrop-blur-md gap-6 pb-14">
+        <h3 className="size-10 flex justify-center items-center text-2xl bg-gray-800 rounded-md font-bold">M</h3>
+        <h3 className='text-2xl font-semibold'>Welcome Back</h3>
+        <p className='text-center'>Enter your credentials to access your personalized news feed</p>
+        <label className="floating-label w-full">
+          <span>Your Email</span>
+          <input
+            type="text"
+            placeholder="mail@site.com"
+            className="input input-md w-full"
+          />
+        </label>
+        <label className="floating-label w-full">
+          <span>Your Password</span>
+          <input
+            type="password"
+            placeholder="********"
+            className="input input-md w-full"
+          />
+        </label>
+        <button className="btn overflow-hidden bg-white/70 backdrop-blur rounded-md text-black w-full">Sign In</button>
+    </div>
+  </div>
   )
 }
 
